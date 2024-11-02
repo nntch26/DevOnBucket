630c0f9cd6e21d7d74d5751b76a9fc3c58231a68
65070117Natcha <nntch26@gmail.com>
2024-11-02 11:53:44 +0700

    login register

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -6,4 +6,9 @@ from . import views
 urlpatterns = [
     path("", views.indexView.as_view(), name="index"),
 
+    # login & register
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+
 ]
