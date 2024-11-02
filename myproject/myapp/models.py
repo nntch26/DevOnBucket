5e249d5e045e9286c68bae5e415e17d790a65889
Peeradech01 <suapeeradech@gmail.com>
2024-11-02 16:05:48 +0700

    Add Post models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -28,3 +28,13 @@ class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+    def __str__(self):
+        return self.title
